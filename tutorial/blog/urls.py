713ca98cabfaecb9aabe16788be29d91b77ea2fa
firefly221 <bbudyn01@gmail.com>
2024-06-13 22:51:01 +0200

    Dodano linki to szczegolow

--- tutorial/blog/urls.py
+++ tutorial/blog/urls.py
@@ -3,7 +3,8 @@ from django.urls import path
 
 
 urlpatterns = [
-    path('index',views.post_list,name = "post_list")
+    path('index',views.post_list,name = "post_list"),
+    path('post/<int:pk>/',views.post_detail,name = "post_detail"),
 ]
 
 
--- tutorial/blog/views.py
+++ tutorial/blog/views.py
@@ -1,9 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Post
 # Create your views here.
 
 def post_list(req):
     posts= Post.objects.all()
     return render(req,'blog/post_list.html',{'posts' : posts})
+
+
+def post_detail(req,pk):
+    post = get_object_or_404(Post,pk=pk)
+    return render(req,'blog/post_detail.html',{'post':post})
+
 
 
